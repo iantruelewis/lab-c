719f2070c49af21668589410ddfc30e30645eae7
iantruelewis <168608288+iantruelewis@users.noreply.github.com>
2026-08-26 20:12:13 -0400

    lab-c | August 2026 | Guessing Game

---
+++ 0826_guessing_game/guessing_game.c
@@ -0,0 +1,36 @@
+// guessing_game
+//////
+
+#include <stdio.h>
+
+int main(void)
+{
+    int secret_number;
+    int guess_number;
+
+    secret_number = 5;
+
+    int i;
+    int guess_limit;
+
+    guess_limit =3;
+
+    for (i = 1; i <= guess_limit; i++)
+    {
+        printf("Guess Number %d : ", i);
+        scanf("%d", &guess_number);
+        
+        if(guess_number == secret_number)
+        {
+            printf("You Won!");
+            break;
+        }
+    }
+
+    if (guess_number != secret_number)
+    {
+        printf("You Lost!");
+    }
+
+    return 0;
+}
